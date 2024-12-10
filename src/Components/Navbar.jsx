d0bdd47944c9f8fc7623b93340b6731d92fb2b2b
olujimiAdebakin <omoladebu231@gmail.com>
2024-12-10 15:55:32 +0100

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,37 @@
 
 import { Link, NavLink } from 'react-router-dom'
-import { FaBarsStaggered } from "react-icons/fa6";
-import { IoIosMoon } from 'react-icons/io';
+import { IoIosMoon, IoMdSunny } from 'react-icons/io';
+import { useEffect, useState } from 'react';
+import { GrClose } from 'react-icons/gr';
+import { SlMenu } from 'react-icons/sl';
 const Navbar = () => {
+
+  const [showNav, setShowNav] = useState(window.innerWidth > 600);
+  const [darkTheme, setDarkTheme] =useState(localStorage.getItem('voting-app-theme'))
+
+
+  // function to close nav menu on small screens when menu link is clicked
+  const closeNavMenu = () => {
+    if (window.innerWidth < 600) {
+      setShowNav(false);
+    } else {
+      setShowNav(true)
+    }
+  };
+
+  const changeThemeHandler = () => {
+    if (localStorage.getItem('voting-app-theme') == 'dark') {
+      localStorage.setItem('voting-app-theme', '')
+    } else {
+      localStorage.setItem('voting-app-theme', 'dark')
+    }
+    setDarkTheme(localStorage.getItem('voting-app-theme'))
+  }
+
+  useEffect(() => {
+    document.body.className = localStorage.getItem('voting-app-theme')
+  }, [darkTheme])
+
   return (
     <>
       <nav>
@@ -11,18 +40,31 @@ const Navbar = () => {
             VOTEX
           </Link>
 
-          <menu>
-            <NavLink to="/elections">Elections</NavLink>
-            <NavLink to="/results">Results</NavLink>
-            <NavLink to="/logout">Logout</NavLink>
-          </menu>
+          <div>
+            {showNav && (
+              <menu>
+                <NavLink to="/elections" onClick={closeNavMenu}>
+                  Elections
+                </NavLink>
+                <NavLink to="/results" onClick={closeNavMenu}>
+                  Results
+                </NavLink>
+                <NavLink to="/logout" onClick={closeNavMenu}>
+                  Logout
+                </NavLink>
+              </menu>
+            )}
 
-          <button className="theme_toggle_btn">
-            <IoIosMoon />
-          </button>
-          <button className="nav_toggle_btn">
-            <FaBarsStaggered />
-          </button>
+            <button className="theme_toggle_btn" onClick={changeThemeHandler}>
+              {darkTheme ? <IoMdSunny/> :  <IoIosMoon /> }
+            </button>
+            <button
+              className="nav_toggle_btn"
+              onClick={() => setShowNav(!showNav)}
+            >
+              {showNav ? <GrClose /> : <SlMenu />}
+            </button>
+          </div>
         </div>
       </nav>
     </>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
+    <>
+      <section className="register">
+        <div className="register_container">
+          <h2>Sign Up</h2>
+          <form>
+            <p className="form_error-message">Any Error from the server</p>
+            <input
+              type="text"
+              placeholder="Full Name"
+              autoComplete="true"
+              autoFocus
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              autoComplete="true"
+              name="Email"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              autoComplete="true"
+              name="password"
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              autoComplete="true"
+              name="password"
+            />
+            <p>
+              Already have an account? <Link href="/">Sign in</Link>
+            </p>
+            <div className="btn primary">Register</div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Register
